003fe836497a7a1d97aa8941fe569b078971f2ac
MarilynWaldman <mdwaldman22@gmail.com>
2022-08-04 04:22:01 -0600

    cleaned up imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
-import React, { RefObject, useEffect, useRef } from 'react';
-import './App.scss';
-import { useRecoilValue } from 'recoil'
-import {  mapObject } from './model'
 
-import { getWorldData } from './recoil/selectors/worldSelectors'
-import * as d3 from 'd3'
-import * as d3geo from 'd3-geo'
+import './App.scss';
 import  D3Map  from './components/D3Map/D3Map'
-import { deflateRaw } from 'zlib';
-//import *  from 'bertin'
 import styled from 'styled-components'
 
 
@@ -22,45 +14,6 @@ const Container = styled.div`
     width: 100vw;
     height: 100vh;
 `
-
-const D5Map = () => {
-
-  const worldData: mapObject = useRecoilValue(getWorldData) as mapObject
-  const ref: RefObject<HTMLDivElement> = React.createRef()
-
-  const thing = bertin.draw({
-    params: {
-       projection: d3geo.geoAlbersUsa(),
-       width: 350,
-       height:300,
-       clip:true
-     },
-    layers: [
-     {type: "simple", geojson: worldData.mapFeatures, 
-          tooltip: ["$ISO3", "$NAMEen"],
-          fill: "blue",
-          fillOpacity: .5,
-         }    
-    ]
-   })
-
-  useEffect(() => {
-    draw()
-  })
-
-  const draw = () => {
-   
-    ref.current?.appendChild(thing)
-    
-  }
-
-  return (
-    <div  ref={ref}/>    
-  )
-}
-
-
-
 
 function App() {
   
