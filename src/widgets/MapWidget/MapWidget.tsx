f2b4b98d9713d3023f48810fe005fa730cf8259b
MarilynWaldman <mdwaldman22@gmail.com>
2022-08-09 18:09:45 -0600

    using atom and selector for map manifest

--- src/widgets/MapWidget/MapWidget.tsx
+++ src/widgets/MapWidget/MapWidget.tsx
@@ -8,23 +8,12 @@ import { manifestState } from '../../recoil/atoms/manifestAtoms';
 
 
 const MapWidget = () => {
-    //const worldData: mapObject = useRecoilValue(getWorldData) as mapObject
-    //const manifestData: manifestObject = useRecoilValue(getManifest) as manifestObject
-    //console.log("Manifest Data")
-    //console.log(manifestData)
-    const [worldmanifest, setWorldmanifest] = useRecoilState(manifestState) 
-    console.log("get manifest state atom")
-    console.log (worldmanifest) 
-    //const x = { ...worldmanifest as any }
-    //console.log("x")
-    //console.log(x)
     
-    //const y = {...x as any, x.layers[0].fill: 'blue' }
-    //setWorldmanifest( x);
-    //console.log("y")
-    //console.log(y)
+    const [worldmanifest, setWorldmanifest] = useRecoilState(manifestState) 
+    //console.log("get manifest state atom")
+    //console.log (worldmanifest) 
 
-    //const newmanifest = () => setWorldmanifest(manifest)
+  
     useEffect(() => {
       // results
        //console.log(worldData.mapFeatures.length)
